dc181df022037b81bfadd015690ae30dc096e0c7
alexhong742 <alexhong742@users.noreply.github.com>
2017-08-25 17:35:09 -0700

    committed server

--- server/remover.js
+++ server/remover.js
@@ -1,9 +1,9 @@
-let findRemoveSync = require('find-remove')
-let path = require('path')
+let findRemoveSync = require('find-remove');
+let path = require('path');
 
 module.exports = (req,res,next) => {
-    let variable = findRemoveSync(path.join(__dirname, './../store/polymerUI'), {dir: "*", files: "*.*"})
-    console.log(`${variable}`)
-}
+    let variable = findRemoveSync(path.join(__dirname, './../store/polymerUI'), {dir: "*", files: "*.*"});
+    console.log(`${variable}`);
+};
 
 
